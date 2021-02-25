d5f19d5fbcae7e33c654562af2f99bc37667d5db
Ben Kramer <benkramer614@gmail.com>
2021-02-25 08:57:46 -0500

    commit tree 9945a55208a614238fe75864679e48ade640e1f2

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1 
+    array.join('')
+    
+    elsif array.length == 2 
+      array.join(' and ')
+      elsif array.length >= 3 
+        array[-1] = "and #{array[-1]}"
+        array.join(', ')
+      else 
+        nil 
+      end 
+  end   
+    
+  oxford_comma(["kiwi"])
